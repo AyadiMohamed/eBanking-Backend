de6df9ccce3c5cf702a7d7afd7d22148b739fabc
Ayadi Mohmaed <ayadimed589@gmail.com>
2023-09-21 12:29:37 +0100

    add services

---
+++ src/main/java/com/ayadi/ebankingbackend/exceptions/BankAccountNotFoundException.java
@@ -0,0 +1,2 @@
+package com.ayadi.ebankingbackend.exceptions;public class BankAccountNotFoundException {
+}
---
+++ src/main/java/com/ayadi/ebankingbackend/services/BankAccountService.java
@@ -0,0 +1,2 @@
+package com.ayadi.ebankingbackend.services;public interface BankAccountService {
+}
